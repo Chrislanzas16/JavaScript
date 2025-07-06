1b6c174381bbc6b1c6f90c118ae768bdb0514618
Chris <chrislanzas89@outlook.com>
2025-07-06 19:26:14 -0400

    Learning Objects in JS

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
-let users = [
-    {
-        username: "David",
-        email: "david@frontendsimplified.com",
-        subscriptionStatus: "VIP",
-        discordId: "David Bragg#0001",
-        lessonsCompleted: [0, 1],
-    },
-    
-    {
-        username: "Mitri",
-        email: "mitri@frontendsimplified.com",
-        subscriptionStatus: "VIP",
-        discordId: "Mitri#0001",
-        lessonsCompleted: [0, 1, 2, 3],
-    },
 
-];
 
-console.log(users[0])
+function register(
+  username,
+  email,
+  password,
+  subscriptionStatus,
+  discordId,
+  lessonsCompleted
+) {
+    let user = {
+      username: username,  
+      email: email ,
+      password:password, 
+      subscriptionStatus,
+      discordId,
+      lessonsCompleted,
+      
+    }
+  console.log(user);
+}
+
+register(
+  "chris",
+  "chris@frontendsimplified.com",
+  "test123",
+  "VIP",
+  "Chris#0001",
+  [1, 2, 3]
+);
