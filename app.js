48c7e4bdc1640565d031850fadc79ba0b5278d24
Chris <chrislanzas89@outlook.com>
2025-07-07 13:40:16 -0400

    Working on DOM

--- app.js
+++ app.js
@@ -1,30 +1,7 @@
+document.querySelector('.title').innerHTML += 'Frontend Simplified'
 
+document.querySelector(".title").style.fontSize = '16px' 
 
-function register(
-  username,
-  email,
-  password,
-  subscriptionStatus,
-  discordId,
-  lessonsCompleted
-) {
-    let user = {
-      username: username,  
-      email: email ,
-      password:password, 
-      subscriptionStatus,
-      discordId,
-      lessonsCompleted,
-      
-    }
-  console.log(user);
-}
-
-register(
-  "chris",
-  "chris@frontendsimplified.com",
-  "test123",
-  "VIP",
-  "Chris#0001",
-  [1, 2, 3]
-);
+ function changeTitleToRed (button) {
+    document.querySelector('.title').style.color = 'red'
+ }
