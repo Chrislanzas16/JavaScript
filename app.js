4f0f4d81577af6dad2278da9b8a96fa4927449a4
Chris <chrislanzas89@outlook.com>
2025-07-07 16:49:07 -0400

    Worked on Promises

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
-document.querySelector('.title').innerHTML += 'Frontend Simplified'
+// fetch("https://jsonplaceholder.typicode.com/users/1")
 
-document.querySelector(".title").style.fontSize = '16px' 
+// 1. Then
+// fetch("https://jsonplaceholder.typicode.com/users/1")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
 
- function changeTitleToRed (button) {
-    document.querySelector('.title').style.color = 'red'
- }
+// async function main() {
+//  const response = await fetch ("https://jsonplaceholder.typicode.com/users/1")
+//  const data = await response.json()
+//  console.log(data)
+//  emailRef.innerHTML = data.email
+// }
+
+const statusRef = document.querySelector ('.status')
+
+function getSubscriptionStatus () {
+    return new Promise ((resolve, reject) => {
+      resolve ("VIP")
+}) }
+
+function getVideo (subscriptionStatus) {
+    return new Promise ((resolve, reject) => {
+        if (subscriptionStatus === "VIP")  {resolve ('show video')}
+       else if (subscriptionStatus === "Free") {resolve ('show trailer')}
+         else {reject ('no video')}
+        
+    } ) 
+    
+}
+
+async function main() {
+    const status = await getSubscriptionStatus();
+   statusRef.innerHTML = status
+   console.log( await getVideo(status));}
+
+
+ main()
